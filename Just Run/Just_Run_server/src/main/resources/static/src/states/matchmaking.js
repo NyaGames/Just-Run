de4302eda4c46b1abcff40b6ec36b6961db12d88
Jaime <43349828+KaceGameDev@users.noreply.github.com>
2018-11-15 11:16:35 +0100

    Animacion pantalla carga

--- Just Run/Just_Run_server/src/main/resources/static/src/states/matchmaking.js
+++ Just Run/Just_Run_server/src/main/resources/static/src/states/matchmaking.js
@@ -1,4 +1,8 @@
 var matchmakingState = function(Just_Run) {	
+	matchmakingState.prototype.preload = function(){
+
+	}
+
 	matchmakingState.prototype.create = function(){
 		$.get("http://localhost:8080/matchmaking", function(data){
 			if(data === 0){
@@ -10,5 +14,12 @@ var matchmakingState = function(Just_Run) {
 		text = "Esperando jugadores..\n"
 	    style = { font: "20px Times New Roman", fill: "#FFFFFF", align: "center" };
 	    spr_text = game.add.text(game.world.centerX-100, 50, text, style);
+
+	    //animacion pantalla carga
+	    var pantallacarga = game.add.sprite(940, 470, 'carga');
+	    pantallacarga.scale.setTo(0.2,0.2);
+		var loading = pantallacarga.animations.add('load');
+		pantallacarga.animations.play('load', 26, true);
 	}
+
 }
